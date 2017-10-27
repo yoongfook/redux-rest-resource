d3ca821d936e7702f745a663b7f8ec4a5e42b846
Olivier Louvignes <olivier@mg-crea.com>
2017-10-27 20:16:38 +0200

    Updates

--- test/setup/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-
-try { require('debug-utils'); } catch (err) {};
-global.Promise = require.requireActual('bluebird');
-global.fetch = require.requireActual('isomorphic-fetch');
-// global.log = require.requireActual('ololog').configure({time: true, stringify: {print: require('q-i').stringify}})
